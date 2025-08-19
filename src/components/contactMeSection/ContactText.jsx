58b11d6e2213d5f8bed897996cde85fd0ba74c7d
fhevila <104440747+fhevila@users.noreply.github.com>
2025-08-19 09:08:52 -0300

    Update ContactText.jsx

--- src/components/contactMeSection/ContactText.jsx
+++ src/components/contactMeSection/ContactText.jsx
@@ -3,8 +3,6 @@ const ContactText = () => {
     <div>
       <h2 className="text-white text-3xl mb-4">Mande mensagem</h2>
       <p>
-        Feel free to reach out if you’d like to collaborate
-        <br />
         Você está a apenas um clique de distância!
       </p>
     </div>
